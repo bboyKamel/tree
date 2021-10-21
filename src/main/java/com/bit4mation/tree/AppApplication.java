336d0bbb1eb10eac0517e7b288650b90fac35a3e
bboyKamel <nacoon@tlen.pl>
2021-10-21 13:58:22 +0200

    initial commit

---
+++ src/main/java/com/bit4mation/tree/AppApplication.java
@@ -0,0 +1,13 @@
+package com.bit4mation.tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bit4mation/tree/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit4mation.tree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
